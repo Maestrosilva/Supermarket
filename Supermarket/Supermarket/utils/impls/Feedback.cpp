feab2b04d9f9ff8c222da5727543daa4c55ab64f
Georgi Mihalev <116490071+Maestrosilva@users.noreply.github.com>
2025-06-02 18:18:30 +0300

    Delete Supermarket/Supermarket/utils/impls/Feedback.cpp

--- Supermarket/Supermarket/utils/impls/Feedback.cpp
+++
@@ -1,8 +0,0 @@
-#include "..//headers//Feedback.h"
-
-void Feedback::getFeedback(const String& command, bool successful) {
-	if (!successful) {
-		std::cout << "Invalid " << command << " command!" << std::endl;
-	}
-
-}
